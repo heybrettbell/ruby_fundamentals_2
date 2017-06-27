2e537cd8896bedc170313f17b35bceab9b95ea8a
heybrettbell <brett@grassrootsonline.ca>
2017-06-27 13:48:17 -0400

    Exercise 5 Part 1 & 2 complete

---
+++ exercise5-2.rb
@@ -0,0 +1,8 @@
+#Part 1
+def greets_backwards(first_name)
+  return "Hello #{first_name.reverse}#{first_name.reverse}! Welcome home."
+end
+  puts greets_backwards('Bob')
+  puts greets_backwards('Shirly')
+  puts greets_backwards('Sue')
+  puts greets_backwards('Andy')
